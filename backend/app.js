65ff607d1a51e5c480521b0753af59bbbe732410
Eddie Gomez <eddieegomez4@gmail.com>
2020-02-22 12:10:03 -0800

    cors

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,13 @@ const obliqueStrategiesRouter = require('./api/routes/obliqueStrategies');
 
 app.use(morgan('dev'));
 
+let allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  next();
+};
+app.use(allowCrossDomain);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
